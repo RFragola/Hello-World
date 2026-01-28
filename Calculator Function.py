0ac4d2cf2ffc78592975e53202e2ffafb089723f
RFragola <robert.fragola@student.fairfield.edu>
2026-01-28 15:56:27 -0500

    Changes

--- Calculator Function.py
+++ Calculator Function.py
@@ -5,3 +5,10 @@ def main():
     number = float(input("Enter a number: "))
     squared = number ** 2
     square_root = number ** 0.5
+
+x = float(input("Number 1: "))
+y = float(input("Number 2: "))
+
+# Calculate the answer and round to 2 decimal places
+z = round(x + y , 2)
+print(f"Sum = {z}")
