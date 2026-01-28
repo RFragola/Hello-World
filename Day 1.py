c7798a6e87ad1d33e2aeb69d5c73f33ee02a3499
RFragola <robert.fragola@student.fairfield.edu>
2026-01-28 15:51:39 -0500

    Calculator

---
+++ Day 1.py
@@ -0,0 +1,4 @@
+# This Program prompts the user for their name and greets them with a personalized message.
+name = input("What's your name? ")
+name = " ".join(name.split()).title() # Normalize whitespace and capitalize each word
+print(f"Hello, {name}!")
